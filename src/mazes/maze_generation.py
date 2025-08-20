d0828004ef1dbefbe9c18787759d8427fcf21cbe
DanielDavis <danieldavis998@gmail.com>
2025-08-21 01:03:00 +1200

    Add first maze solver and better ascii output

--- src/mazes/maze_generation.py
+++ src/mazes/maze_generation.py
@@ -2,80 +2,17 @@ import logging
 
 import numpy as np
 
+from src.mazes.maze import DIRECTIONS, Maze, rng
+
 LOG = logging.getLogger('MazeGeneration')
 logging.basicConfig(level='INFO')
 LOG.setLevel('INFO')
 
-rng = np.random.default_rng()
 
-DIRECTIONS = {
-    'N': np.array([-1, 0]),
-    'W': np.array([0, -1]),
-    'S': np.array([1, 0]),
-    'E': np.array([0, 1]),
-}
-
-
-class Maze:
-    def __init__(self, dimensions: tuple[int, int]):
-        self.rows = dimensions[0]
-        self.cols = dimensions[1]
-
-        self.maze_array = np.ones((*dimensions, 4))
-        # self.maze_array = np.random.random((*dimensions, 4)) > 0.5
-
-        self.direction_layer_map = {
-            'N': 0,
-            'W': 1,
-            'S': 2,
-            'E': 3,
-        }
-
-        self.node_set = set([(i, j) for i in range(self.rows) for j in range(self.cols)])
-
-    def move(self, node, direction):
-        return tuple(np.array(node) + DIRECTIONS[direction])
-
-    def remove_wall(self, node: tuple[int, int], direction: str) -> None:
-        dest_node = self.move(node, direction)
-
-        layer = self.direction_layer_map[direction]
-        reverse_layer = (layer + 2) % 4
-
-        self.maze_array[*node, layer] = 0
-        self.maze_array[*dest_node, reverse_layer] = 0
-
-    def is_wall(self, node, direction):
-        return self.maze_array[*node, self.direction_layer_map[direction]]
-
-    def to_string(self) -> str:
-        """
-        puts " " + "_" * (width * 2 - 1)
-        height.times do |y|
-          print "|"
-          width.times do |x|
-            print((grid[y][x] & S != 0) ? " " : "_")
-            if grid[y][x] & E != 0
-              print(((grid[y][x] | grid[y][x+1]) & S != 0) ? " " : "_")
-            else
-              print "|"
-            end
-          end
-          puts
-        end
-        """
-        maze_string = ''
-        for i in range(self.rows):
-            maze_string += '+' + '+'.join('---' if self.is_wall((i, j), 'N') else '   ' for j in range(self.cols)) + '+\n'
-            maze_string += ''.join('|   ' if self.is_wall((i, j), 'W') else '    ' for j in range(self.cols)) + ('|' if self.is_wall((i, self.cols - 1), 'E') else ' ') + '\n'
-        maze_string += '+' + '+'.join('---' if self.is_wall((self.rows - 1, j), 'S') else '   ' for j in range(self.cols)) + '+'
-        return maze_string
-
-
-def recursive_backtrack(maze, current_node=None, loop_chance: float = 0.02):
+def recursive_backtrack(maze, current_node=None, loop_chance: float = 0.0):
     LOG.debug('Creating maze using `recursive_backtrack`')
     if current_node is None:
-        current_node = tuple(rng.integers((maze.rows, maze.cols), size=2))
+        current_node = maze.random_node()
     LOG.debug(f'Visiting node: {current_node}')
 
     directions = np.array(list(DIRECTIONS.keys()))
@@ -84,14 +21,14 @@ def recursive_backtrack(maze, current_node=None, loop_chance: float = 0.02):
         next_node = maze.move(current_node, direction)
         if next_node in maze.node_set:  # Is it a legal node?
             # Is it an unvisited node, or should we randomly add a loop?
-            if maze.maze_array[*next_node, :].all() or rng.random() < loop_chance:
+            if (maze.maze_array[*next_node, :] == float('inf')).all() or rng.random() < loop_chance:
                 maze.remove_wall(current_node, direction)
                 recursive_backtrack(maze, next_node)
 
 
-def iterative_backtrack(maze: Maze, loop_chance: float = 0.02):
+def iterative_backtrack(maze: Maze, loop_chance: float = 0.0):
     LOG.info('Creating maze using `iterative_backtrack`')
-    current_node = tuple(rng.integers((maze.rows, maze.cols), size=2))
+    current_node = maze.random_node()
     move_stack = [current_node]
     directions = np.array(list(DIRECTIONS.keys()))
 
@@ -103,7 +40,7 @@ def iterative_backtrack(maze: Maze, loop_chance: float = 0.02):
             next_node = maze.move(current_node, direction)
             if next_node in maze.node_set:  # Is it a legal node?
                 # Is it an unvisited node, or should we randomly add a loop?
-                if maze.maze_array[*next_node, :].all() or rng.random() < loop_chance:
+                if (maze.maze_array[*next_node, :] == float('inf')).all() or rng.random() < loop_chance:
                     maze.remove_wall(current_node, direction)
                     move_stack.append(current_node)
                     current_node = next_node
@@ -111,6 +48,46 @@ def iterative_backtrack(maze: Maze, loop_chance: float = 0.02):
         else:  # Didn't break, backtrack
             current_node = move_stack.pop()
             continue
+
+
+def ellers(maze: Maze):
+    pass
+
+
+def kruskals(maze: Maze):
+    pass
+
+
+def prims(maze: Maze):
+    pass
+
+
+def recursive(maze: Maze):
+    pass
+
+
+def aldous_broder(maze: Maze):
+    pass
+
+
+def wilsons(maze: Maze):
+    pass
+
+
+def hunt_and_kill(maze: Maze):
+    pass
+
+
+def growing_tree(maze: Maze):
+    pass
+
+
+def brinary_tree(maze: Maze):
+    pass
+
+
+def sidewinder(maze: Maze):
+    pass
 
 
 if __name__ == '__main__':
---
+++ src/mazes/pathfinding.py
@@ -0,0 +1,74 @@
+import logging
+
+from src.mazes.maze import Maze, Node
+
+LOG = logging.getLogger('PathFinding')
+logging.basicConfig(level='INFO')
+LOG.setLevel('INFO')
+
+
+def reconstruct_path(move_map, goal) -> list:
+    node = goal
+
+    path = [node]
+    while (node := move_map[node]) is not None:
+        path.append(node)
+
+    return list(reversed(path))
+
+
+def dijkstras(maze: Maze, start: Node, finish: Node = None):
+    node_set: set = maze.node_set.copy()
+
+    move_map = {node: None for node in node_set}
+    g_score = {node: float('inf') for node in node_set}
+
+    g_score[start] = 0
+
+    while node_set and any(score < float('inf') for score in g_score.values()):
+        current_code = min(node_set, key=lambda n: g_score[n])
+        node_set.remove(current_code)
+
+        if finish and current_code == finish:
+            break
+
+        for node, cost in maze.get_neighbours(current_code):
+            if g_score[current_code] + cost < g_score[node]:
+                g_score[node] = g_score[current_code] + cost
+                move_map[node] = current_code
+
+    return g_score, move_map
+
+
+def a_star(maze: Maze):
+    pass
+
+
+def weighted_a_star(maze: Maze, weight):
+    pass
+
+
+def breadth_first_search(maze: Maze):
+    pass
+
+
+def depth_first_search(maze: Maze):
+    pass
+
+
+if __name__ == '__main__':
+    from src.mazes.maze import AcsiiView
+    from src.mazes.maze_generation import iterative_backtrack as maze_maker
+    maze_solver = dijkstras
+
+    maze = Maze((11, 11))
+    maze_maker(maze)
+    start = (0, 5)  # maze.random_node()
+    maze.remove_wall(start, 'N')
+
+    LOG.info(f"Beginning search at {start}")
+    g_scores, move_map = maze_solver(maze, start)
+
+    finish = max(g_scores, key=lambda n: g_scores[n])
+    LOG.info(f"Reached {finish} with a cost of {g_scores[finish]}")
+    print(AcsiiView(maze, start, finish, reconstruct_path(move_map, finish)))
